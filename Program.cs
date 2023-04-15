a035ab19244104a3c1e0522def5ce90ceaeea298
Dave Thompson <davethompson59@aol.com>
2023-04-16 00:48:37 +0100

    Added Connection Helper to check if environment is Local or Production

--- Program.cs
+++ Program.cs
@@ -5,12 +5,14 @@ using Microsoft.EntityFrameworkCore;
 using AddressBookPro.Services;
 using AddressBookPro.Services.Interfaces;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using AddressBookPro.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+//var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
