da1899e3d945e42defe59cc4c404573da6bf559d
Dave Thompson <davethompson59@aol.com>
2023-04-15 15:25:48 +0100

    Deleted the Action & View for unused Category Details

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -43,7 +43,7 @@ namespace AddressBookPro.Controllers
             return View(categories);
         }
 
-        //GET: EmailCategory
+        // GET: EmailCategory
         [Authorize]
         public async Task<IActionResult> EmailCategory(int id)
         {
@@ -90,27 +90,6 @@ namespace AddressBookPro.Controllers
                 }
             }
             return View(ecvm);
-        }
-
-
-        // GET: Categories/Details/5
-        [Authorize]
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
         }
 
         // GET: Categories/Create
