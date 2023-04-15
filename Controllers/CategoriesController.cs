2d7b2fe0b924a9e3f8d381756c567099d99565a3
Dave Thompson <davethompson59@aol.com>
2023-04-15 11:20:43 +0100

    Added Code to Get Send Button working

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using AddressBookPro.Data;
 using AddressBookPro.Models;
 using AddressBookPro.Models.ViewModels;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace AddressBookPro.Controllers
 {
@@ -17,18 +18,23 @@ namespace AddressBookPro.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IEmailSender _emailService;
 
         public CategoriesController(ApplicationDbContext context,
-                                    UserManager<AppUser> userManager)
+                                    UserManager<AppUser> userManager,
+                                    IEmailSender emailSender)
         {
             _context = context;
             _userManager = userManager;
+            _emailService = emailSender;
         }
 
         // GET: Categories
         [Authorize]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string swalMessage = null)
         {
+            ViewData["SwalMessage"] = swalMessage;
+
             string appUserId = _userManager.GetUserId(User);
             
             var categories = await _context.Categories.Where(c => c.AppUserId == appUserId)
@@ -37,6 +43,7 @@ namespace AddressBookPro.Controllers
             return View(categories);
         }
 
+        //GET: EmailCategory
         [Authorize]
         public async Task<IActionResult> EmailCategory(int id)
         {
@@ -63,6 +70,28 @@ namespace AddressBookPro.Controllers
 
             return View(model);
         }
+
+        // POST: EmailCategory
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> EmailCategory(EmailCategoryViewModel ecvm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                await _emailService.SendEmailAsync(ecvm.EmailData.EmailAddress, ecvm.EmailData.Subject, ecvm.EmailData.Body);
+                return RedirectToAction("Index", "Categories", new { swalMessage = "Success: Email Sent!" });
+                }
+                catch (Exception)
+                {
+                    return RedirectToAction("Index", "Categories", new { swalMessage = "Error: Email Send Failed!" });
+                    throw;
+                }
+            }
+            return View(ecvm);
+        }
+
 
         // GET: Categories/Details/5
         [Authorize]
