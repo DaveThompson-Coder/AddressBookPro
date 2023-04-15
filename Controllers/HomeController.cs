5113166fc77fba165f4e1fa074b0f1f906200f61
Dave Thompson <davethompson59@aol.com>
2023-04-15 16:04:26 +0100

    Custom Error Page Added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,25 @@ namespace AddressBookPro.Controllers
             return View();
         }
 
+        [Route("/Home/HandleError/{code:int}")]
+        public IActionResult HandleError(int code)
+        {
+            var customError = new CustomError();
+
+            customError.code = code;
+
+            if (code == 404)
+            {
+                customError.message = "The page you are looking for might have been removed, had it's name changed or is temporarily unavailable";
+            }
+            else
+            {
+                customError.message = "Sorry, Something went Wrong";
+            }
+
+            return View("~/Views/Shared/CustomError.cshtml", customError);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
